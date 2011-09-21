44dedc1a4bec8c3e237229f784252abdd9b053e3
Erin Sheldon <erin.sheldon@gmail.com>
2011-09-20 21:59:33 -0400

    starting branch with cfitsio 3240 bundled, a version I know works

--- setup.py
+++ setup.py
@@ -2,18 +2,47 @@ import distutils
 from distutils.core import setup, Extension, Command
 import os
 import numpy
+import glob
+
+cfitsio_version = '3240'
+cfitsio_dir = 'cfitsio%s' % cfitsio_version
+makefile = os.path.join(cfitsio_dir, 'Makefile')
+
+def configure_cfitsio():
+    os.chdir(cfitsio_dir)
+    ret=os.system('./configure')
+    if ret != 0:
+        raise ValueError("could not configure cfitsio %s" % cfitsio_version)
+    os.chdir('..')
+
+def compile_cfitsio():
+    os.chdir(cfitsio_dir)
+    ret=os.system('make')
+    if ret != 0:
+        raise ValueError("could not compile cfitsio %s" % cfitsio_version)
+    os.chdir('..')
+
+if not os.path.exists(makefile):
+    configure_cfitsio()
+compile_cfitsio()
 
 data_files=[]
 
+
+sources = ["fitsio/fitsio_pywrap.c"]
+
+extra_objects = glob.glob(os.path.join(cfitsio_dir,'*.o'))
 ext=Extension("fitsio._fitsio_wrap", 
-              ["fitsio/fitsio_pywrap.c"],
-              libraries=['cfitsio'])
+              sources,
+              extra_objects=extra_objects)
+
+include_dirs=[cfitsio_dir,numpy.get_include()]
 setup(name="fitsio", 
       packages=['fitsio'],
       version="1.0",
       data_files=data_files,
       ext_modules=[ext],
-      include_dirs=numpy.get_include())
+      include_dirs=include_dirs)
 
 
 
