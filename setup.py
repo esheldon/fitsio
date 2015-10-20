9b9e1109ffb8364c85e604a8f192f289439d06c4
Dustin Lang <dstndstn@gmail.com>
2015-10-20 09:24:12 -0400

    apply old bzip2 patch from pull request #15

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ else:
     extra_link_args=[]
 ext=Extension("fitsio._fitsio_wrap", 
               sources,
+              libraries=['bz2'],
               extra_objects=extra_objects,
               extra_compile_args=extra_compile_args, 
               extra_link_args=extra_link_args)
