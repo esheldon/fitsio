a3b834a983d81ee3bf4f98ef1a7563133e4a0afc
Matthew R Becker <becker.mr@gmail.com>
2019-04-19 14:39:20 -0500

    move a few more things

--- fitsio/__init__.py
+++ fitsio/__init__.py
@@ -10,17 +10,16 @@ from . import fitslib   # noqa
 
 from .fitslib import (    # noqa
     FITS,
-    FITSHDR,
     read,
     read_header,
     read_scamp_head,
     write,
     READONLY,
     READWRITE,
-    BINARY_TBL, ASCII_TBL, IMAGE_HDU,
 )
 
-from .header import FITSRecord, FITSCard  # noqa
+from .header import FITSHDR, FITSRecord, FITSCard  # noqa
+from .hdu import BINARY_TBL, ASCII_TBL, IMAGE_HDU  # noqa
 
 from . import util  # noqa
 from .util import cfitsio_version, FITSRuntimeWarning  # noqa
