a663f4bc7a4e18a59b6324789f2b9e71606db62c
Erin Sheldon <erin.sheldon@gmail.com>
2019-06-20 13:29:44 -0400

    bump version

--- setup.py
+++ setup.py
@@ -272,7 +272,7 @@ classifiers = [
 
 setup(
     name="fitsio",
-    version="1.0.4",
+    version="1.0.5",
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown; charset=UTF-8; variant=GFM',
