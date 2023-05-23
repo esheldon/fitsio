b0e141cf23c9f04cafca563846ba5ceff6f3b296
Erin Sheldon <erin.sheldon@gmail.com>
2023-05-23 16:41:40 -0400

    more tests

--- fitsio/tests/checks.py
+++ fitsio/tests/checks.py
@@ -8,3 +8,26 @@ def check_header(header, rh):
             rv = rv.strip()
 
         assert v == rv, "testing equal key '%s'" % k
+
+
+def compare_headerlist_header(header_list, header):
+    """
+    The first is a list of dicts, second a FITSHDR
+    """
+    for entry in header_list:
+        name = entry['name'].upper()
+        value = entry['value']
+        hvalue = header[name]
+
+        if isinstance(hvalue, str):
+            hvalue = hvalue.strip()
+
+        assert value == hvalue, (
+            "testing header key '%s'" % name
+        )
+
+        if 'comment' in entry:
+            assert (
+                entry['comment'].strip() ==
+                header.get_comment(name).strip()
+            ), "testing comment for header key '%s'" % name
--- fitsio/tests/test_header.py
+++ fitsio/tests/test_header.py
@@ -1,9 +1,9 @@
 import os
 import tempfile
 import numpy as np
-from .makedata import lorem_ipsum
-from .checks import check_header
-from ..fitslib import FITS
+from .makedata import make_data, lorem_ipsum
+from .checks import check_header, compare_headerlist_header
+from ..fitslib import FITS, read_header
 from ..header import FITSHDR
 
 
@@ -38,6 +38,23 @@ def test_add_delete_and_update_records():
     assert hdr['Third_record'] == '3'
     assert hdr.get_comment('First_record') == 'number 1'
     assert not hdr.get_comment('New_record')
+
+
+def testHeaderCommentPreserved():
+    """
+    Test that the comment is preserved after resetting the value
+    """
+
+    l1 = 'KEY1    =                   77 / My comment1'
+    l2 = 'KEY2    =                   88 / My comment2'
+    hdr = FITSHDR()
+    hdr.add_record(l1)
+    hdr.add_record(l2)
+
+    hdr['key1'] = 99
+    assert hdr.get_comment('key1') == 'My comment1', (
+        'comment not preserved'
+    )
 
 
 def test_header_write_read():
@@ -81,34 +98,191 @@ def test_header_update():
     with tempfile.TemporaryDirectory() as tmpdir:
         fname = os.path.join(tmpdir, 'test.fits')
 
-            with fitsio.FITS(fname,'rw',clobber=True) as fits:
-                data=numpy.zeros(10)
-                header1={
-                    'SCARD':'one',
-                    'ICARD':1,
-                    'FCARD':1.0,
-                    'LCARD':True
-                }
-                header2={
-                    'SCARD':'two',
-                    'ICARD':2,
-                    'FCARD':2.0,
-                    'LCARD':False,
+        with FITS(fname, 'rw') as fits:
+            data = np.zeros(10)
+            header1 = {
+                'SCARD': 'one',
+                'ICARD': 1,
+                'FCARD': 1.0,
+                'LCARD': True
+            }
+            header2 = {
+                'SCARD': 'two',
+                'ICARD': 2,
+                'FCARD': 2.0,
+                'LCARD': False,
 
-                    'SNEW':'two',
-                    'INEW':2,
-                    'FNEW':2.0,
-                    'LNEW':False
-                }
-                fits.write_image(data, header=header1)
-                rh = fits[0].read_header()
-                self.check_header(header1, rh)
+                'SNEW': 'two',
+                'INEW': 2,
+                'FNEW': 2.0,
+                'LNEW': False
+            }
+            fits.write_image(data, header=header1)
+            rh = fits[0].read_header()
+            check_header(header1, rh)
 
-                # Update header
-                fits[0].write_keys(header2)
+            # Update header
+            fits[0].write_keys(header2)
 
-            with fitsio.FITS(fname) as fits:
-                rh = fits[0].read_header()
-                self.check_header(header2, rh)
+        with FITS(fname) as fits:
+            rh = fits[0].read_header()
+            check_header(header2, rh)
 
 
+def test_read_header_case():
+    """
+    Test read_header with and without case sensitivity
+
+    The reason we need a special test for this is because
+    the read_header code is optimized for speed and has
+    a different code path
+    """
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        fname = os.path.join(tmpdir, 'test.fits')
+
+        with FITS(fname, 'rw') as fits:
+            data = np.zeros(10)
+            _, keys, _, _, _, _ = make_data()
+            fits.write_image(data, header=keys, extname='First')
+            fits.write_image(data, header=keys, extname='second')
+
+        cases = [
+            ('First', True),
+            ('FIRST', False),
+            ('second', True),
+            ('seConD', False),
+        ]
+        for ext, ci in cases:
+            h = read_header(fname, ext=ext, case_sensitive=ci)
+            compare_headerlist_header(keys, h)
+
+
+def test_blank_key_comments():
+    """
+    test a few different comments
+    """
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        fname = os.path.join(tmpdir, 'test.fits')
+
+        with FITS(fname, 'rw') as fits:
+            records = [
+                # empty should return empty
+                {'name': None, 'value': '', 'comment': ''},
+                # this will also return empty
+                {'name': None, 'value': '', 'comment': ' '},
+                # this will return exactly
+                {'name': None, 'value': '', 'comment': ' h'},
+                # this will return exactly
+                {'name': None, 'value': '', 'comment': '--- test comment ---'},
+            ]
+            header = FITSHDR(records)
+
+            fits.write(None, header=header)
+
+            rh = fits[0].read_header()
+
+            rrecords = rh.records()
+
+            for i, ri in ((0, 6), (1, 7), (2, 8)):
+                rec = records[i]
+                rrec = rrecords[ri]
+
+                assert rec['name'] is None, (
+                    'checking name is None'
+                )
+
+                comment = rec['comment']
+                rcomment = rrec['comment']
+                if '' == comment.strip():
+                    comment = ''
+
+                assert comment == rcomment, (
+                    "check empty key comment"
+                )
+
+
+def test_blank_key_comments_from_cards():
+    """
+    test a few different comments
+    """
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        fname = os.path.join(tmpdir, 'test.fits')
+
+        with FITS(fname, 'rw') as fits:
+            records = [
+                '                                                                                ',  # noqa
+                '         --- testing comment ---                                                ',  # noqa
+                '        --- testing comment ---                                                 ',  # noqa
+                "COMMENT testing                                                                 ",  # noqa
+            ]
+            header = FITSHDR(records)
+
+            fits.write(None, header=header)
+
+            rh = fits[0].read_header()
+
+            rrecords = rh.records()
+
+            assert rrecords[6]['name'] is None, (
+                'checking name is None'
+            )
+            assert rrecords[6]['comment'] == '', (
+                'check empty key comment'
+            )
+            assert rrecords[7]['name'] is None, (
+                'checking name is None'
+            )
+            assert rrecords[7]['comment'] == ' --- testing comment ---', (
+                "check empty key comment"
+            )
+            assert rrecords[8]['name'] is None, (
+                'checking name is None'
+            )
+            assert rrecords[8]['comment'] == '--- testing comment ---', (
+                "check empty key comment"
+            )
+            assert rrecords[9]['name'] == 'COMMENT', (
+                'checking name is COMMENT'
+            )
+            assert rrecords[9]['comment'] == 'testing', (
+                "check comment"
+            )
+
+
+def test_header_from_cards():
+    """
+    test generating a header from cards, writing it out and getting
+    back what we put in
+    """
+    hdr_from_cards = FITSHDR([
+        "IVAL    =                   35 / integer value                                  ",  # noqa
+        "SHORTS  = 'hello world'                                                         ",  # noqa
+        "UND     =                                                                       ",  # noqa
+        "LONGS   = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiu&'",  # noqa
+        "CONTINUE  'smod tempor incididunt ut labore et dolore magna aliqua'             ",  # noqa
+        "DBL     =                 1.25                                                  ",  # noqa
+    ])
+    header = [
+        {'name': 'ival', 'value': 35, 'comment': 'integer value'},
+        {'name': 'shorts', 'value': 'hello world'},
+        {'name': 'und', 'value': None},
+        {'name': 'longs', 'value': lorem_ipsum},
+        {'name': 'dbl', 'value': 1.25},
+    ]
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        fname = os.path.join(tmpdir, 'test.fits')
+
+        with FITS(fname, 'rw') as fits:
+            data = np.zeros(10)
+            fits.write_image(data, header=hdr_from_cards)
+
+            rh = fits[0].read_header()
+            compare_headerlist_header(header, rh)
+
+        with FITS(fname) as fits:
+            rh = fits[0].read_header()
+            compare_headerlist_header(header, rh)
