f687b018296a40e8611f38bc5256a9b0b4b1c164
Erin Sheldon <erin.sheldon@gmail.com>
2023-05-23 21:00:18 -0400

    BUG don't set zero qlevel
    gzip doesn't work with qlevel 0

--- fitsio/fitsio_pywrap.c
+++ fitsio/fitsio_pywrap.c
@@ -1480,8 +1480,10 @@ PyFITSObject_create_image_hdu(struct PyFITSObject* self, PyObject* args, PyObjec
                 goto create_image_hdu_cleanup;
             }
 
-            if (fits_set_quantize_level(self->fits, qlevel, &status)) {
-                goto create_image_hdu_cleanup;
+            if (qlevel > 0) {
+                if (fits_set_quantize_level(self->fits, qlevel, &status)) {
+                    goto create_image_hdu_cleanup;
+                }
             }
 
             if (fits_set_quantize_method(self->fits, qmethod, &status)) {
