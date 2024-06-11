5899be0b4eec20147adca709150207b4613fa52e
Erin Sheldon <erin.sheldon@gmail.com>
2024-06-11 11:55:24 -0400

    more empty slice tests, update change log

--- fitsio/tests/test_empty_slice.py
+++ fitsio/tests/test_empty_slice.py
@@ -5,15 +5,17 @@ from ..fitslib import write, FITS
 
 
 def test_empty_image_slice():
+
     shape = (10, 10)
     data = np.arange(shape[0] * shape[1]).reshape(shape)
+
     with tempfile.TemporaryDirectory() as tmpdir:
         fname = os.path.join(tmpdir, 'test.fits')
         write(fname, data, clobber=True)
 
-        # third, test slices using fitsio
         with FITS(fname) as fits:
-            # first, passing the slices directly
-            # overlap = f[0][xslice, yslice]
-            overlap = fits[0][0:8, 0:0]
-            assert overlap.size == 0
+            assert fits[0][0:0, 0:0].size == 0
+
+            assert fits[0][0:8, 0:0].size == 0
+
+            assert fits[0][0:0, 0:8].size == 0
