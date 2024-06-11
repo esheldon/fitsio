e05f3b6a686f862eb493544428271db4bd1b6e14
Erin Sheldon <erin.sheldon@gmail.com>
2024-06-11 11:47:21 -0400

    BUG wrong image file path

--- fitsio/tests/test_empty_slice.py
+++ fitsio/tests/test_empty_slice.py
@@ -7,13 +7,12 @@ from ..fitslib import write, FITS
 def test_empty_image_slice():
     shape = (10, 10)
     data = np.arange(shape[0] * shape[1]).reshape(shape)
-    outfile = 'test_image.fits'
     with tempfile.TemporaryDirectory() as tmpdir:
         fname = os.path.join(tmpdir, 'test.fits')
         write(fname, data, clobber=True)
 
         # third, test slices using fitsio
-        with FITS(outfile) as fits:
+        with FITS(fname) as fits:
             # first, passing the slices directly
             # overlap = f[0][xslice, yslice]
             overlap = fits[0][0:8, 0:0]
