3caab9898dfdfad0ed66ebe2fe17e9d1b6450481
beckermr <becker.mr@gmail.com>
2025-10-30 10:31:04 -0500

    test: ensure test works on ascii tables

--- fitsio/tests/test_table.py
+++ fitsio/tests/test_table.py
@@ -1586,15 +1586,20 @@ def test_table_big_col(table_type):
 
 @pytest.mark.parametrize("table_type", ["binary", "ascii"])
 def test_table_null_end_strings(table_type):
-    d = np.ones(1, dtype=[("blah", "U70")])
-    d["blah"] = "".join(["a"] * 60)
+    d = np.ones(2, dtype=[("blah", "U70")])
+    d["blah"][0] = "".join(["a"] * 60)
+    d["blah"][1] = ""
     with tempfile.TemporaryDirectory() as tmpdir:
         pth = os.path.join(tmpdir, "test.fits")
         write(pth, d, table_type=table_type)
         data = read(pth)
-        np.testing.assert_array_equal(d, data)
         assert len(data["blah"][0]) == 60
         assert "U70" in data["blah"].dtype.descr[0][1]
+
+        if table_type == "ascii":
+            # null strings in ascii tables are a single blank
+            d["blah"][1] = " "
+        np.testing.assert_array_equal(d, data)
 
 
 def test_table_read_write_ulonglong():
