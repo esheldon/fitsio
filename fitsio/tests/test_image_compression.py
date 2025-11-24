46ae2c2f09832f5722397969c7e5016a0862c09f
beckermr <becker.mr@gmail.com>
2025-11-24 07:02:42 -0600

    test: add proper skip

--- fitsio/tests/test_image_compression.py
+++ fitsio/tests/test_image_compression.py
@@ -618,6 +618,12 @@ def test_image_mem_reopen_noop():
     ],
 )
 def test_image_compression_big_gzip(coef):
+    if coef == 2 and CFITSIO_VERSION < 4.4:
+        pytest.skip(
+            "Writing compressed binary tables exceeding "
+            "2*32 bytes fails for cfitsio < 4.40!"
+        )
+
     n1 = 50
     n2 = 50
     nHDU = 10
