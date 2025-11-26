251fdf1d1a7a66883bac2fe6de04027447e4da85
beckermr <becker.mr@gmail.com>
2025-11-26 12:21:33 -0600

    test: expand test to ensure the GIL is released

--- fitsio/tests/test_threading.py
+++ fitsio/tests/test_threading.py
@@ -1,6 +1,7 @@
 from multiprocessing.pool import ThreadPool
 import os
 import tempfile
+import time
 
 import numpy as np
 import fitsio
@@ -11,6 +12,9 @@ def test_threading():
     Test a basic image write, data and a header, then reading back in to
     check the values
     """
+
+    size = 3000
+
     with tempfile.TemporaryDirectory() as tmpdir:
         filenames = [
             os.path.join(tmpdir, "fname%d.fits" % i) for i in range(32)
@@ -19,7 +23,7 @@ def test_threading():
         # create files for reading in serial
         def create_file(i):
             fname = filenames[i]
-            data = np.zeros((32, 32), dtype='f8')
+            data = np.zeros((size, size), dtype='f8')
             data[:] = i
             with fitsio.FITS(fname, 'rw', clobber=True) as fits:
                 fits.write_image(data)
@@ -29,6 +33,21 @@ def test_threading():
             with fitsio.FITS(fname, 'r') as fits:
                 assert (fits[0].read() == i).all()
 
+        t0 = time.time()
         with ThreadPool(32) as pool:
             pool.map(create_file, range(32))
             pool.map(read_file, range(32))
+        t0_threads = time.time() - t0
+        print("threaded time:", t0_threads, flush=True)
+
+        t0 = time.time()
+        for i in range(32):
+            create_file(i)
+            read_file(i)
+        t0_serial = time.time() - t0
+        print("serial time:", t0_serial, flush=True)
+
+        assert t0_threads < t0_serial, (
+            "Threading should be faster than serial! ( %f < %f)"
+            % (t0_threads, t0_serial)
+        )
