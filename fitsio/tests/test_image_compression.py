fd544f2f84c1a937c69185bc28bd19e61610176b
beckermr <becker.mr@gmail.com>
2026-07-01 15:21:56 -0500

    test: move this test to the slow ones and run slow once on old code

--- fitsio/tests/test_image_compression.py
+++ fitsio/tests/test_image_compression.py
@@ -610,6 +610,7 @@ def test_image_mem_reopen_noop():
         assert np.array_equal(rimg, img)
 
 
+@pytest.mark.slow
 @pytest.mark.parallel_threads_limit(1)
 @pytest.mark.iterations(1)
 @pytest.mark.parametrize(
